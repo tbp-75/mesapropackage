b676760c5666e0be560bd5a7f888fb8df394712d
tbp-75 <61416652+tbp-75@users.noreply.github.com>
2020-10-31 19:36:05 +0100

    Update run.py
    -

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from mesa import app
 
 # If application is called directly from the command line
 # it automatically runs on debug mode
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
